894aff4d9103f249f6e82e123506d913ba2627ab
Dmitry <dmitriy@Bono-Dmitry-Smirnov-Vladimirovich.local>
2020-07-27 15:50:04 +0300

    fix

--- src/js/modules/webgl/director.js
+++ src/js/modules/webgl/director.js
@@ -50,37 +50,54 @@ const sceneInit = () => {
   const car = carWrap.children[0]
 
   const direct = new THREE.Vector3(0, 0, 0)
-  let angle = 45 * Math.PI / 2
+  let angle = 0
+  let angleTarget = 0
   let speed = 0
+  let speedTarget = 0
 
   const moveCar = () => {
+    speed = g.l.lerp(speed, speedTarget, 0.04)
+    angle = g.l.lerp(angle, angleTarget, 0.04)
+
     direct.x = Math.sin(angle)
     direct.z = Math.cos(angle)
     direct.normalize()
     direct.multiplyScalar(speed)
     carWrap.position.add(direct)
     car.rotation.y = angle
+
+    console.log(speed);
   }
 
   g.l.addLoop('moveCar', () => {
     moveCar()
   })
 
-  document.addEventListener('keydown', (evt) => {
-    if (evt.key === 'ArrowUp') {
-      speed += 0.1
+  const pressed = new Set()
+
+  window.addEventListener('keydown', (evt) => {
+    pressed.add(evt.key)
+  })
+
+  window.addEventListener('keyup', (evt) => {
+    pressed.delete(evt.code)
+  })
+
+  g.l.addLoop('setCarTarget', () => {
+    if (pressed.has('ArrowUp')) {
+      speedTarget = 0.4
+    } else if (pressed.has('ArrowDown')) {
+      speedTarget = -0.2
+    } else {
+      speedTarget = 0
     }
 
-    if (evt.key === 'ArrowDown') {
-      speed -= 0.1
+    if (pressed.has('ArrowLeft')) {
+      angleTarget += 0.03 * speed / 0.2
     }
 
-    if (evt.key === 'ArrowLeft') {
-      angle += 0.1
-    }
-
-    if (evt.key === 'ArrowRight') {
-      angle -= 0.1
+    if (pressed.has('ArrowRight')) {
+      angleTarget -= 0.03 * speed / 0.2
     }
   })
 }
