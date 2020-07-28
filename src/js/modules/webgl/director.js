a21d0ab1900c81dfa76c4cb4c31e95d3ff5a7c68
Dmitry <dmitriy@Bono-Dmitry-Smirnov-Vladimirovich.local>
2020-07-28 13:18:43 +0300

    fix

--- src/js/modules/webgl/director.js
+++ src/js/modules/webgl/director.js
@@ -41,10 +41,10 @@ const sceneInit = () => {
   const gridHelper = new THREE.GridHelper(100, 100, 0x0000ff, 0x808080)
   g.v.scene.add(gridHelper)
 
-  const planeG = new THREE.PlaneGeometry(5, 20, 32)
-  const planeM = new THREE.MeshStandardMaterial({ color: 0xffff00, side: THREE.DoubleSide })
-  const meshPlane = new THREE.Mesh(planeG, planeM)
-  g.v.scene.add(meshPlane)
+  const geometry = new THREE.BoxGeometry( 2, 2, 2 );
+  const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+  const cube = new THREE.Mesh( geometry, material );
+  g.v.scene.add( cube )
 
   g.v.camera.position.set(20, 20, 20)
   g.v.controls.update()
@@ -52,8 +52,7 @@ const sceneInit = () => {
   const light = new THREE.HemisphereLight(0xCCCCCC, 0x666666, 1)
   g.v.scene.add(light)
 
-  const carWrap = g.v.scene.children[1]
-  const car = carWrap.children[0]
+  const car = g.v.scene.children[1]
 
   const direct = new THREE.Vector3(0, 0, 0)
   let angle = 0
@@ -69,8 +68,10 @@ const sceneInit = () => {
     direct.z = Math.cos(angle)
     direct.normalize()
     direct.multiplyScalar(speed)
-    carWrap.position.add(direct)
-    car.rotation.y = angle
+    // carWrap.position.add(direct)
+    body.position.vadd(new CANNON.Vec3(direct.x, direct.y, direct.z), body.position)
+    body.quaternion.setFromAxisAngle(new CANNON.Vec3(0, 1, 0), angle)
+    // car.rotation.y = angle
   }
 
   g.l.addLoop('moveCar', () => {
@@ -108,7 +109,7 @@ const sceneInit = () => {
   const world = new CANNON.World()
   world.gravity.set(0, -9, 0)
   world.broadphase = new CANNON.NaiveBroadphase()
-  // world.solver.iterations = 10;
+  world.solver.iterations = 10
 
   // ground plane
   const groundMaterial = new CANNON.Material()
@@ -117,19 +118,26 @@ const sceneInit = () => {
     mass: 0,
     material: groundMaterial
   })
+
   groundBody.addShape(groundShape)
   groundBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2)
   groundBody.position.set(0, 0, 0);
   world.add(groundBody)
-  const shape = new CANNON.Box(new CANNON.Vec3(1, 1, 1))
+  const shape = new CANNON.Box(new CANNON.Vec3(car.children[5].scale.x / 2, car.children[5].scale.y / 2, car.children[5].scale.z / 2))
   const body = new CANNON.Body({
     mass: 400,
     position: new CANNON.Vec3(1, 10, 1)
   })
   body.addShape(shape)
-  // body.angularVelocity.set(0, 10, 0)
-  // body.angularDamping = 0.5
   world.addBody(body)
+
+  const shape2 = new CANNON.Box(new CANNON.Vec3(1, 1, 1))
+  const body2 = new CANNON.Body({
+    mass: 1000,
+    position: new CANNON.Vec3(0, 1, 0)
+  })
+  body2.addShape(shape2)
+  world.addBody(body2)
 
   const timeStep = 1 / 60
 
@@ -140,5 +148,9 @@ const sceneInit = () => {
     // Copy coordinates from Cannon.js to Three.js
     car.position.copy(body.position)
     car.quaternion.copy(body.quaternion)
+
+    cube.position.copy(body2.position)
+    cube.quaternion.copy(body2.quaternion)
+
   })
 }
